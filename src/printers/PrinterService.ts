ba4eae26161e48218fe627d5609eacf293ed1c38
Krisztian Kemenes <kemkriszt@yahoo.com>
2023-12-31 16:42:10 +0200

    Added 'requestPrinter'

--- src/printers/PrinterService.ts
+++ src/printers/PrinterService.ts
@@ -1,4 +1,4 @@
-import { UsbDevice, getDevices } from "@/helpers/USBUtils"
+import { UsbDevice, getDevices, requestDevice } from "@/helpers/USBUtils"
 import TSPLPrinter from "./TSPLPrinter"
 import Printer from "./Printer"
 
@@ -27,5 +27,19 @@ export class PrinterService {
         const devices = await getDevices()
         const optionalPrinters = await Promise.all(devices.map(PrinterService.printerForDevice))
         return optionalPrinters.filter(printer => !!printer) as Printer[]
+    }
+
+    /**
+     * This is intended to be used in the browser although it also works in node
+     * In the browser, it provides a UI for the user to choose a USB device and if it is a
+     * printer, it returns a new printer instance. In Node however, this will try to create
+     * a printer for the first available device and returns it. This means that even if there is a 
+     * printer, it may return undefined. In node, use `getPrinters` instead
+     */
+    static async requestPrinter(): Promise<Printer|undefined> {
+        const device = await requestDevice()
+        if(device) {
+            return PrinterService.printerForDevice(device)
+        }
     }
 }
