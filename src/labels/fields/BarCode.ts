8645aee578629e55af32b1a5f2238039b7390044
Krisztian Kemenes <kemkriszt@yahoo.com>
2024-01-21 19:52:41 +0200

    Update BarCode.ts

--- src/labels/fields/BarCode.ts
+++ src/labels/fields/BarCode.ts
@@ -9,23 +9,31 @@ export default class BarCode extends LabelField {
     private readonly y: number
     private readonly type: BarcodeType
     private readonly height: number
-    private readonly rotation: Rotation
-    private readonly humanReadable: BarcodeHumanReable 
+    private rotation: Rotation
+    private humanReadable: BarcodeHumanReable 
     private readonly alignment: Alignment
 
-    constructor(content: string, x: number, y: number, type: BarcodeType, height: number, rotatio: Rotation, humanReadable: BarcodeHumanReable, alignment: Alignment) {
+    constructor(content: string, x: number, y: number, type: BarcodeType, height: number) {
         super()
         this.content = content
         this.x = x
         this.y = y
         this.type = type
         this.height = height
-        this.rotation = rotatio
-        this.humanReadable = humanReadable
-        this.alignment = alignment
+        this.rotation = 0
+        this.humanReadable = "none"
+        this.alignment = "left"
     }
 
-    async commandForLanguage(language: PrinterLanguage, config?: PrintConfig | undefined): Promise<Command> {
+    setRotation(rotation: Rotation) {
+        this.rotation = rotation
+    }
+
+    setHumanReadable(humanReadable: BarcodeHumanReable) {
+        this.humanReadable = humanReadable
+    }
+
+    async commandForLanguage(language: PrinterLanguage, _config?: PrintConfig | undefined): Promise<Command> {
         return await this.commandGeneratorFor(language).barCode(this.content, this.x, this.y, this.type, this.height, this.rotation, this.humanReadable, this.alignment)
     }
 }
